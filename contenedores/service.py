556d6f4a130976aa85b67f6455874771a596b65e
sandra <sandra.kourtsounioti@gmail.com>
2018-01-13 20:15:51 +0100

    update service file

--- contenedores/service.py
+++ contenedores/service.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def test():
     return jsonify({"status":"OK"})
 
+@app.route('/status/')
+def testStatus():
+    return jsonify({'status':'OK'})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
